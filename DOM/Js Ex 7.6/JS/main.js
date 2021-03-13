82eed848b27ea2361109ccb11b637d8a8e637734
BazaochiDumitru <54941010+BazaochiDumitru@users.noreply.github.com>
2021-03-13 18:59:08 +0200

    Update main.js

--- DOM/Js Ex 7.6/JS/main.js
+++ DOM/Js Ex 7.6/JS/main.js
@@ -1,8 +1,6 @@
 // EX. 7.6
 // Добавить в страницу таблицу 8*8. При помощи JS кода закрасить 
 // цвет фона чтобы получилась шахматная доска.
-// Использовать querySelector*
-
 
 for (let i = 0; i < 64; i++) {
   document.getElementById("sah").appendChild(document.createElement("div")).style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? 'black' : 'white';
