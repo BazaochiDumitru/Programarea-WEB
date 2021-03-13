6c396077177b7a140252dee54be273ae8748dcd9
BazaochiDumitru <54941010+BazaochiDumitru@users.noreply.github.com>
2021-03-13 18:58:47 +0200

    Update main.js

--- DOM/Js Ex 7.6/JS/main.js
+++ DOM/Js Ex 7.6/JS/main.js
@@ -4,6 +4,6 @@
 // Использовать querySelector*
 
 
-for (var i = 0; i < 64; i++) {
+for (let i = 0; i < 64; i++) {
   document.getElementById("sah").appendChild(document.createElement("div")).style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? 'black' : 'white';
 }
