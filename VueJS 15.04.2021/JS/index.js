2870758ac2bc7e6116e8ae82664f2f4d1e79c2bc
BazaochiDumitru <54941010+BazaochiDumitru@users.noreply.github.com>
2021-04-15 13:56:07 +0300

    Add files via upload

--- VueJS 15.04.2021/JS/index.js
+++ VueJS 15.04.2021/JS/index.js
@@ -8,92 +8,71 @@ console.log("Hello world!");
 3. Выведите в шаблон значение этой переменной */
 
 const app = new Vue({
-    el: '#app',
-    data: {
-        username: 'Dumitru'
-    }
-}) 
+  el: "#app",
+  data: {
+    username: "Dumitru",
+  },
+});
 
 /* Задача № 2 - вычисляемые свойства и работа с моделью
 В том же экземпляре приложения, добавьте инпут для выбора даты рождения. 
 Создайте вычисляемое свойство age, в которой будет возраст в годах. Вывести возрас в шаблон. */
 
 const app2 = new Vue({
-    el: '#app2',
-    data: {
-        year: new Date(),
+  el: "#app2",
+  data: {
+    year: new Date(),
+  },
+  computed: {
+    age() {
+      return new Date().getFullYear() - new Date(this.year).getFullYear();
     },
-    computed: {
-        age() {
-            return new Date().getFullYear() - new Date(this.year).getFullYear();
-        }
-    }
-})
-
+  },
+});
 
 // Задача № 3 - отслеживание изменений переменной
 
 const app3 = new Vue({
-    el: '#app3',
-    data: {
-        Javascript: 1,
-        Python: 0.7,
-        Java: 1.3,
-        Experience: 0,
-        salary: 0
-        
+  el: "#app3",
+  data: {
+    experience: "",
+    language: "",
+    salary: 0,
+  },
+  watch: {
+    experience() {
+      this.salary = this.calcSalary(this.language, this.experience);
     },
-    computed: {
+    language() {
+      this.salary = this.calcSalary(this.language, this.experience);
     },
-    watch: {
-        
-    }
-})
-// V-for
+  },
+  methods: {
+    calcSalary(language, experience) {
+      let coefficient = 0;
+      if (language === "JS") {
+        coefficient = 1;
+      } else if (language === "Python") {
+        coefficient = 0.7;
+      } else if (language === "Java") {
+        coefficient = 1.3;
+      }
 
-/* var app = new Vue({
-    el: '#app',
-    data: {
-        students: [
-            "Lungu Andrei",
-            "Maidacenco Anastasia",
-            "Maneev Dumitru",
-            "Mihalachi Alexandrina",
-            "Munteanu Victor"
-        ]
-    }
-}) */
+      let basicRate = 0;
 
-// V-if
+      if (experience === "1") {
+        basicRate = 10000;
+      } else if (experience === "2") {
+        basicRate = 12000;
+      } else if (experience === "3") {
+        basicRate = 13500;
+      } else if (experience === "4") {
+        basicRate = 14000;
+      } else if (experience === "5") {
+        basicRate = 15000;
+      }
 
-/* var app = new Vue({
-    el: '#app',
-    data: {
-        gender: "M"
-    }
-}) */
-
-// Computed + - * /
-
-/* var app = new Vue({
-    el: '#app',
-    data: {
-        a: 0,
-        b: 0,
-        operand: '+'
+      return basicRate * coefficient;
     },
-    computed: {
-        suma() {
-            return parseInt(this.a) + parseInt(this.b);
-        },
-        multiplication() {
-            return parseInt(this.a) * parseInt(this.b);
-        },
-        diference() {
-            return parseInt(this.a) - parseInt(this.b);
-        },
-        divide() {
-            return parseInt(this.a) / parseInt(this.b);
-        }
-    }
-}) */
+  },
+});
